84f3b726521537d133c4f510967f40af478bf65a
Nafikova_Julia <nafikova91@yandex.ru>
2024-10-06 00:00:40 +0300

    structure

---
+++ src/services.py
@@ -0,0 +1,3 @@
+def get_increased_cashback(data, year, month):
+    '''функция для анализа выгодности категорий повышенного кешбэка'''
+    pass
---
+++ src/utils.py
@@ -0,0 +1,28 @@
+def get_greeting():
+    '''Приветствие в зависимости от текущего времени'''
+    pass
+
+def get_cards_last_digits():
+    '''Получает последние 4 цифры карты'''
+    pass
+
+def get_cards_total_spent():
+    '''Получает общую сумму расходов'''
+
+def get_cards_cashback():
+    '''Получает кешбэк (1 рубль на каждые 100 рублей)'''
+    pass
+
+def get_top_transactions():
+    '''Получает Топ-5 транзакций по сумме платежа'''
+    pass
+
+def get_currency_rates():
+    '''Получает курс валют'''
+    pass
+
+def get_stock_prices():
+    '''Получает стоимость акций из S&P500'''
+    pass
+
+
